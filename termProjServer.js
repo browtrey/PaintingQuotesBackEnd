7ee3469a2fb0d4df18e46c1d2b0ce93d545c5bfe
Trey Browne <browtrey@sheridancollege.ca>
2022-04-15 19:50:52 -0400

    final commit

--- termProjServer.js
+++ termProjServer.js
@@ -15,9 +15,44 @@ app.use((req, res, next) => {
 app.listen(port, () => console.log(`Server running at localhost:
 ${port}!`))
 
-///////make it independent/////////////
+
 const mongoose = require('mongoose');
+
+//connects to database
+/*
+app.get('/setup', (req, res) => {
+  const databaseName = req.query.db
+  const collectionName = req.query.col
+  console.log("db: " + databaseName + "------" + "col: " + collectionName)
+
+  const DataBase = 'mongodb://localhost:27017/' + databaseName;
+  console.log(DataBase)
+  const quoteSchema = new mongoose.Schema({
+    Id: Number,
+    qDate: String,
+    qName: String,
+    qAddress: String,
+    qEmail: String,
+    qNumofRooms: Number,
+    qRoom: [{
+      roomType: String,
+      roomWidth: Number,
+      roomLength: Number,
+      roomColour: String,
+      roomPaintType: String
+    }]
+  });
+  console.log(quoteSchema)
+  const quote = mongoose.model(collectionName, quoteSchema);
+  mongoose.connect(DataBase, { useNewUrlParser: true, useUnifiedTopology: true });
+  const db = mongoose.connection;
+  db.on('error', (err) => { console.log(err); })
+  res.send("database created")
+
+});
+*/
 const DataBase = 'mongodb://localhost:27017/quotations';
+console.log(DataBase)
 const quoteSchema = new mongoose.Schema({
   Id: Number,
   qDate: String,
@@ -34,13 +69,12 @@ const quoteSchema = new mongoose.Schema({
   }]
 });
 const quote = mongoose.model('quote', quoteSchema);
-mongoose.connect(DataBase, { useNewUrlParser: false, useUnifiedTopology: false });
+mongoose.connect(DataBase, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', (err) => { console.log(err); })
-////////////////////////////////////
 
 db.once('open', () => {
-
+  //adds to database
   app.post('/add', (req, res) => {
     input = req.body.params;
     quote.create(input)
@@ -52,6 +86,7 @@ db.once('open', () => {
       .catch(err => { console.log(err); });
   });
 
+  //lists all objects in a database
   app.get('/listAll', (req, res) => {
     quote.find()
       .then(
@@ -62,6 +97,7 @@ db.once('open', () => {
       .catch(err => { console.log(err); });
   });
 
+  //list a single object in database
   app.get('/listOne', (req, res) => {
     input = req.query;
     console.log(input)
@@ -77,6 +113,7 @@ db.once('open', () => {
       .catch(err => { console.log(err); });
   });
 
+  //deletes all objects in database
   app.delete('/deleteAll', (res) => {
     quote.deleteMany()
       .then(
@@ -87,9 +124,11 @@ db.once('open', () => {
       .catch(err => { console.log(err); });
   });
 
+  //deletes a single object
   app.delete('/deleteOne', (req, res) => {
-    input = req.query;
-    quote.deleteOne(input)
+    input = req.query.Id;
+    console.log(input)
+    quote.deleteOne({_id: input})
       .then(
         result => {
           console.log("One Record Deleted");
@@ -98,11 +137,12 @@ db.once('open', () => {
       .catch(err => { console.log(err); });
   });
 
+  //updates a single object
   app.put('/update', (req, res) => {
     id = req.body.params[0].id;
     console.log(id)
     newInfo = req.body.params[1];
-    quote.updateOne({_id: id}, { $set: newInfo })
+    quote.updateOne({ _id: id }, { $set: newInfo })
       .then(
         result => {
           res.send(result);
